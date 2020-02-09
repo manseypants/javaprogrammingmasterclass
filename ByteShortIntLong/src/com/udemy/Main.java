e34d21d5b61538c2ea7270834304a7bd3b87cfff
Ryan Manse <ryanmanse@gmail.com>
2020-02-09 14:40:10 -0600

    add byte short int long datatypes/casting

--- ByteShortIntLong/src/com/udemy/Main.java
+++ ByteShortIntLong/src/com/udemy/Main.java
@@ -6,16 +6,50 @@ public class Main {
 
         int myValue = 1000;
 
-        int myMinIntValue = Integer.MIN_VALUE;
+        int myMinIntValue = Integer.MIN_VALUE; //wrapper class (literal value)
         int myMaxIntValue = Integer.MAX_VALUE;
 
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
 
-        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1)); //the integer number is too large if you add 1
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
-        int myMaxIntTest = 2_147_483_647; //the integer number is too large if you add 1
+        int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        long myLongValue = 100L; //the "l" forces java to treat the value as a long
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+
+        long bigLongLiteralValue = 2_147_483_647_234L;
+
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
+        int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte)(myMinByteValue / 2);
+
+        short myNewShortValue = (short) (myMinShortValue / 2);
+
+
 
     }
 }
