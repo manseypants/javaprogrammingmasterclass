2b1c2742b0364b8939a3a728974eaff5f7bc460b
Ryan Manse <ryanmanse@gmail.com>
2020-02-09 13:23:51 -0600

    add byte short int and long

---
+++ ByteShortIntLong/src/com/udemy/Main.java
@@ -0,0 +1,21 @@
+package com.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 1000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2_147_483_647; //the integer number is too large if you add 1
+
+    }
+}
