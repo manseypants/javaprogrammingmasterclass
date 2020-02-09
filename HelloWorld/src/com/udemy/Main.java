2d262156fcdf124d5f81e8d6be0416f28ca019d4
Ryan Manse <ryanmanse@gmail.com>
2020-02-09 11:56:09 -0600

    add hello world excercise

---
+++ HelloWorld/src/com/udemy/Main.java
@@ -0,0 +1,13 @@
+package com.udemy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Ryan");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+
+        System.out.println(myFirstNumber);
+
+    }
+}
