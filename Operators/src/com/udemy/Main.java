31c6d960d2611bd25000e1a048ab36a28f4f5399
manseypants <ryan.manse@perficient.com>
2020-02-12 15:05:50 -0600

    update finished operator course and challenge complete

--- Operators/src/com/udemy/Main.java
+++ Operators/src/com/udemy/Main.java
@@ -44,6 +44,54 @@ public class Main {
         //result = result - 2
         result -= 2; //10 - 2 = 8
         System.out.println("10 - 2 = " + result);
-        
+
+        boolean isAlien = false; // one "=" is an assignment operator
+        if (isAlien == false) { // two "==" is a comparison between the operands
+            System.out.println("it is not an alien!"); //if you don't mke a code block then only one line can be added
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if(topScore !=100) { //you can use >= or <= or anything as the operator
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 81;
+        if((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100.");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true.");
+        }
+
+        int newValue = 50;
+        if(newValue == 50) {
+            System.out.println("This is true");
+        }
+
+        boolean isCar = false;
+        if(isCar) {
+            System.out.println("This is not supposed to happen!");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if(wasCar) {
+            System.out.println("wasCar is true.:");
+        }
+
+        double variable1 = 20.00;
+        double variable2 = 80.00;
+        double variable3 = ((variable1 + variable2) * 100.00);
+        System.out.println("My values total " + variable3);
+        double variable4 = variable3 % 40.00;
+        System.out.println("The remainder equals " + variable4);
+        boolean trueValue = variable4 == 0;
+        if(!trueValue) {
+            System.out.println("Got some remainder value");
+        }
+
     }
+
 }
